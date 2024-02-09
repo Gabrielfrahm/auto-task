f35125895429a570958f3929d00501dbb5d95e9d
Gabriel Frahm <gabriel_frahm@hotmail.com>
2024-02-09 08:08:11 -0300

    fix(remove): remove files

--- src/infra/nestjs/src/app.module.ts
+++ src/infra/nestjs/src/app.module.ts
@@ -1,20 +1,9 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { DrizzleConnection } from '../../orm/drizzle/connection';
 import { ConfigModule } from '@nestjs/config';
 import { TaskModule } from './module/task.module';
 @Module({
   imports: [ConfigModule.forRoot({ isGlobal: true }), TaskModule],
-  controllers: [AppController],
-  providers: [
-    AppService,
-    {
-      provide: 'drizzleConnection',
-      useFactory: async () => {
-        return DrizzleConnection.getInstance();
-      },
-    },
-  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
